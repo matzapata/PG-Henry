ca2334fcbdde506da7c9dd1cf663ee4350ba21d7
Matias Zapata <42716817+matzapata@users.noreply.github.com>
2022-08-19 10:27:36 -0300

    refactor(App.ts): Fix typos and rename page components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import FormSingUp from "./pages/SingUp";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Tournaments from "./pages/Tournaments";
-import Login from "./pages/Login";
+
+import HomePage from "./pages/Home";
+import NotFoundPage from "./pages/NotFound";
+import TournamentsPage from "./pages/Tournaments";
+import SignUpPage from "./pages/SingUp";
+import LoginPage from "./pages/Login";
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/torneos" component={Tournaments} />
-      <Route exact path="/auth/login" component={Login} />
-      <Route exact path="/auth/signup" component={FormSingUp} />
-      <Route exact path="/" component={Home} />
-      <Route path="*" component={NotFound} />
+      <Route exact path="/torneos" component={TournamentsPage} />
+      <Route exact path="/auth/login" component={LoginPage} />
+      <Route exact path="/auth/signup" component={SignUpPage} />
+      <Route exact path="/" component={HomePage} />
+      <Route path="*" component={NotFoundPage} />
     </Switch>
   );
 }
