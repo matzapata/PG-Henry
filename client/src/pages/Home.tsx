2733a96177839c47fba457ee3bff204a6b26d241
Matias Zapata <matuzapata@gmail.com>
2022-08-21 10:57:54 -0300

    feat(client/pages/Home.tsx): landing page text and header

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 
 function Home() {
   return (
     <>
       <NavBar />
-      <Box maxWidth="4xl" mx={["4", "auto"]} mt="20">
-        <Text fontSize="xl" fontWeight="medium" mb="2">
-          About us
-        </Text>
-        <Text>
+      <Box maxWidth="3xl" mx={["4", "8", "auto"]} mt="20">
+        <Heading
+          fontSize={["3xl", "5xl"]}
+          fontWeight="800"
+          color="purple.700"
+          textAlign="center"
+        >
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut explicabo
+          quisquam.
+        </Heading>
+        <Text mt="6" textAlign="center" fontSize="lg">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Malesuada sed
           habitasse felis, volutpat nunc adipiscing mi, suspendisse. Nec
           ultrices sed lectus nunc fringilla. Tellus varius hendrerit nunc in
