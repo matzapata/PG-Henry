e217bc3b65682ac66b23403076aadccd66309748
Melina Maccio <melina.maccio416@gmail.com>
2022-08-22 10:56:21 -0300

    fix(client/src/components) : fix auth0, styles

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Image } from "@chakra-ui/react";
 
 function Logo({ ...props }) {
-  return <Image src="/img/logo.png" {...props} />;
+  return (
+    <Image src="/img/logo.png" {...props} w="10" h="10" borderRadius={20} />
+  );
 }
 
 export default Logo;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,44 +1,49 @@
 import React from "react";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Container, Heading, Text } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 
 function Home() {
   return (
-    <>
+    <Container
+      maxW="100vw"
+      h="950px"
+      p="0"
+      bgSize="cover"
+      bgImage="url('https://www.xtrafondos.com/wallpapers/uefa-champions-league-estadio-2932.jpg')"
+    >
       <NavBar />
       <Box
         px={["4", "24"]}
         py={["10", "24"]}
         mx={["4", "10"]}
         my="4"
-        borderRadius={8}
-        backgroundColor="purple.500"
+        mt="15%"
+        borderRadius="20px"
+        opacity="95%"
+        backgroundColor="#082032"
       >
         <Box maxWidth="3xl" mx={["4", "8", "auto"]}>
           <Heading
             fontSize={["2xl", "5xl"]}
             fontWeight="800"
-            color="purple.100"
+            color="#F7F7F7"
             textAlign="center"
           >
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
-            explicabo quisquam.
+            Prode Master, la mejor página de pronósticos deportivos
           </Heading>
           <Text
             mt="6"
             textAlign="center"
             fontSize={["md", "lg"]}
             fontWeight="500"
-            color="purple.100"
+            color="#F7F7F7"
           >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Malesuada
-            sed habitasse felis, volutpat nunc adipiscing mi, suspendisse. Nec
-            ultrices sed lectus nunc fringilla. Tellus varius hendrerit nunc in
-            iaculis eget. Quam volutpat donec auctor dictum.
+            Participá de los torneos mas famosos por premios en efectivo, o creá
+            el tuyo personalizado para competir con tus amigos
           </Text>
         </Box>
       </Box>
-    </>
+    </Container>
   );
 }
 
