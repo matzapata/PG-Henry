baa7b9ad35d1e0184a392e6bd17fb4fbce7545f7
Melina Maccio <melina.maccio416@gmail.com>
2022-08-24 16:25:29 -0300

    Ruta Get TournamentMatches

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -2,11 +2,13 @@ import * as express from "express";
 import userRouter from "./users";
 import tournamentRouter from "./tournaments";
 import authRouter from "./auth";
+import matchesRouter from "./matches";
 
 const router: express.Router = express.Router();
 
 router.use("/users", userRouter);
 router.use("/auth", authRouter);
 router.use("/tournaments", tournamentRouter);
+
 
 export default router;
