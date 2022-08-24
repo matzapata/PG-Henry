f4bdb02a3da316120b437e4f7ffbc71432797653
Xxmbxxmb <diegocerdacelis@hotmail.com>
2022-08-24 12:01:25 -0400

    Feature(add route for changing password, with jwt verification)

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -1,6 +1,9 @@
 import * as express from "express";
 import db from "../db";
 import * as bcrypt from "bcryptjs";
+import { protectedRoute } from "../middleware/auth";
+import { verifyAccessToken } from "../utils/jwt";
+import { JwtPayload } from "jsonwebtoken";
 
 const router: express.Router = express.Router();
 
@@ -10,23 +13,45 @@ router.get("/", (req: express.Request, res: express.Response) => {
 
 router.put(
   "/changepsw",
+  protectedRoute,
   async (req: express.Request, res: express.Response) => {
     const { email, password } = req.body;
     if (!email || !password)
       return res.status(400).send("Faltan parametros requeridos!");
+
+    const token = req.headers["x-access-token"];
+    if (!token) return res.status(403).send({ message: "No token provided!" });
+
     try {
-      const user = await db.user.update({
-        where: {
-          email: email,
-        },
-        data: {
-          password: bcrypt.hashSync(password, 8),
-        },
-      });
-      res.status(200).json({ msg: "Contraseña cambiada exitosamente." });
+      const decoded = (await verifyAccessToken(token as string)) as JwtPayload;
+      req.user = {
+        id: decoded.payload.id,
+        email: decoded.payload.email,
+        username: decoded.payload.username,
+      };
+
+      const user: any = await db.user.findUnique({ where: { email } });
+
+      if (
+        user.email === req.user.email &&
+        email === req.user.email &&
+        user.id === req.user.id
+      ) {
+        const update_user = await db.user.update({
+          where: {
+            email: email,
+          },
+          data: {
+            password: bcrypt.hashSync(password, 8),
+          },
+        });
+        return res
+          .status(200)
+          .json({ msg: "Contraseña cambiada exitosamente." });
+      }
     } catch (err) {
       console.error(err);
-      res.status(400).json({ error: "No se ha podido cambiar la contraseña." });
+      res.status(400).json({ error: "Ingresaste un mail incorrecto." });
     }
   }
 );
