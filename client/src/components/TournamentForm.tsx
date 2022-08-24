9d03a39a602d2e6f047c10cd3160d46c0e346fa8
AlejandroGuil <alejandro.i.guil@gmail.com>
2022-08-24 15:51:51 -0300

    refactor{client/component/TournamentForm.tsx}

--- client/src/components/TournamentForm.tsx
+++ client/src/components/TournamentForm.tsx
@@ -1,9 +1,4 @@
-import React, {
-  FieldsetHTMLAttributes,
-  ReactEventHandler,
-  useEffect,
-  useState,
-} from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   Container,
@@ -12,7 +7,6 @@ import {
   Stack,
   Input,
   Button,
-  Divider,
   Textarea,
   Select,
   Flex,
@@ -21,18 +15,16 @@ import {
   InputGroup,
   InputLeftElement,
   NumberInput,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
   NumberInputField,
-  Image,
   GridItem,
-  SelectProps,
   FormErrorMessage,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { check } from "prettier";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { useHistory } from "react-router-dom";
+import TeamAdd from "./TeamAdd";
+import MatchAdd from "./MatchAdd";
 //import teamSlice from "../redux/slices/teamSlice";
 
 /*model Tournament {
@@ -106,15 +98,9 @@ type Inputs = {
   logo_url: string;
   stage: string;
   password: string;
-  teamName: string;
-  teamShield: string;
-  teamKey: number;
+
   teams: Team[];
-  matches: Matches[];
-  matchDate: string;
-  team_a_name: string;
-  team_b_name: string;
-  matchKey: number;
+  matches: Match[];
 };
 
 type Team = {
@@ -123,28 +109,18 @@ type Team = {
   key: number;
 };
 
-type Matches = {
+type Match = {
   key: number;
   team_a_name: string;
   team_b_name: string;
   date: string;
 };
 
-const validateTeamNames = (teams: Team[], newName: string) => {
-  const result = teams.map((team) => {
-    return team.name === newName ? true : false;
-  });
-  if (result.includes(true)) {
-    alert("ya existe");
-    return false;
-  }
-  return true;
-};
-
 export default function TournamentForm(): JSX.Element {
   const history = useHistory();
   //const currentTeams = useAppSelector((state) => state.teams);
   const dispatch = useAppDispatch();
+  const [suma, setSuma] = useState(0);
   const [showPassword, setShowPassword] = useState(false);
   const [input, setInput] = useState<Inputs>({
     name: "",
@@ -156,16 +132,15 @@ export default function TournamentForm(): JSX.Element {
     logo_url: "",
     stage: "",
     password: "",
-    teamName: "",
-    teamShield: "",
-    teamKey: 0,
     teams: [],
     matches: [],
-    matchDate: "",
-    team_a_name: "",
-    team_b_name: "",
-    matchKey: 0,
   });
+  function addTeam(newTeams: Team[]) {
+    setInput({ ...input, teams: newTeams });
+  }
+  function addMatch(newMatch: Match[]) {
+    setInput({ ...input, matches: newMatch });
+  }
   const cambiosEnInput = (
     e:
       | React.FormEvent<HTMLInputElement>
@@ -177,88 +152,7 @@ export default function TournamentForm(): JSX.Element {
       [e.currentTarget.name]: e.currentTarget.value,
     });
   };
-  const agregaEquipo = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (validateTeamNames(input.teams, input.teamName)) {
-      setInput({
-        ...input,
-        teams: [
-          ...input.teams,
-          {
-            name: input.teamName,
-            shield: input.teamShield,
-            key: input.teamKey,
-          },
-        ],
-        teamName: "",
-        teamShield: "",
-        teamKey: input.teamKey + 1,
-      });
-    }
-  };
-  const quitarEquipo = (e: any) => {
-    const newInputTeam = input.teams.filter((el) => {
-      return el.key != e.target.value;
-    });
 
-    setInput({ ...input, teams: newInputTeam });
-  };
-  const agregaPartido = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    setInput({
-      ...input,
-      matches: [
-        ...input.matches,
-        {
-          team_a_name: input.team_a_name,
-          team_b_name: input.team_b_name,
-          date: input.matchDate,
-          key: input.teamKey,
-        },
-      ],
-      teamName: "",
-      teamShield: "",
-      teamKey: input.teamKey + 1,
-    });
-  };
-  const quitarPartido = (e: any) => {
-    const newInputMatch = input.matches.filter((el) => {
-      return el.key != e.target.value;
-    });
-
-    setInput({ ...input, matches: newInputMatch });
-  };
-  const llenarSelect = (teams: Team[]) => {
-    const selectA: any = document.getElementById("team_a");
-    const selectB: any = document.getElementById("team_b");
-    for (let i = selectA?.options.length; i >= 0; i--) {
-      selectA.remove(i);
-      selectB.remove(i);
-    }
-    for (let i = 0; i < teams.length; i++) {
-      const aTag = document.createElement("option");
-      const aTag2 = document.createElement("option");
-      aTag.setAttribute(teams[i].name, teams[i].name);
-      aTag2.setAttribute(teams[i].name, teams[i].name);
-
-      aTag.innerHTML = teams[i].name;
-      aTag2.innerHTML = teams[i].name;
-
-      selectA?.appendChild(aTag);
-      selectB?.appendChild(aTag2);
-    }
-    setInput({
-      ...input,
-      team_a_name: selectA.value,
-      team_b_name: selectB.value,
-    });
-  };
-
-  useEffect(() => {
-    llenarSelect(input.teams);
-  }, [input.teams]);
-  console.log(input);
   return (
     <Container>
       <Flex>
@@ -269,7 +163,7 @@ export default function TournamentForm(): JSX.Element {
           alignItems="center"
           justifyContent="space-between"
           p="20px"
-          backgroundColor="rgba(57,91,100,0.9)"
+          backgroundColor="rgba(57,91,100,0.98)"
         >
           <Stack spacing="9px">
             <Stack direction="row" spacing={4}>
@@ -342,147 +236,10 @@ export default function TournamentForm(): JSX.Element {
                 </InputGroup>
               </FormControl>
             </Flex>
+            <TeamAdd cb={addTeam} />
 
-            <Box>
-              <form onSubmit={agregaEquipo}>
-                <Text>Agregar Equipos</Text>
-                <Stack direction="row" spacing={4}>
-                  <Input
-                    type="text"
-                    name="teamName"
-                    value={input.teamName}
-                    placeholder="Nombre"
-                    onChange={cambiosEnInput}
-                  />
-                  <Input
-                    type="text"
-                    name="teamShield"
-                    value={input.teamShield}
-                    placeholder="Escudo"
-                    onChange={cambiosEnInput}
-                  />
-                  <Button type="submit">Agregar</Button>
-                </Stack>
-              </form>
-
-              {!!input.teams.length &&
-                input.teams.map((el) => (
-                  <Box key={el.key}>
-                    <GridItem
-                      _hover={{
-                        bgColor: "#04879C",
-                      }}
-                      boxShadow="dark-lg"
-                      transition="200ms ease"
-                      backgroundColor="rgba(57,91,100,0.7)"
-                      borderRadius="20px"
-                      display={"flex"}
-                      p="5px"
-                      w="auto"
-                      margin="5px"
-                    >
-                      <Image
-                        src={el.shield}
-                        w="4rem"
-                        h="4rem"
-                        fit="cover"
-                        borderRadius={"20px"}
-                      />
-
-                      <Stack p="5px" spacing={3}>
-                        <Text fontSize="15px" fontWeight="bold" color="#AEFEFF">
-                          {el.name}
-                        </Text>
-                      </Stack>
-                      <Button value={el.key} onClick={quitarEquipo}>
-                        X
-                      </Button>
-                    </GridItem>
-                  </Box>
-                ))}
-            </Box>
-            <Box>
-              {/*  /////////////////PARTIDOS////////////////// */}
-
-              <form onSubmit={agregaPartido}>
-                <Text>Agregar Partidos</Text>
-
-                <FormControl
-                  isInvalid={
-                    input.team_a_name !== input.team_b_name ||
-                    !input.teams.length
-                      ? false
-                      : true
-                  }
-                >
-                  <Stack direction="row" spacing={4}>
-                    <Select
-                      id="team_a"
-                      name="team_a_name"
-                      onChange={cambiosEnInput}
-                    ></Select>
-                    <Text>Vs.</Text>
-                    <Select
-                      id="team_b"
-                      name="team_b_name"
-                      onChange={cambiosEnInput}
-                    ></Select>
-                    <Input
-                      type="date"
-                      name="date"
-                      value={input.matchDate}
-                      onChange={cambiosEnInput}
-                    />
-                    <Button
-                      type="submit"
-                      disabled={
-                        !input.teams.length ||
-                        input.team_a_name === input.team_b_name
-                          ? true
-                          : false
-                      }
-                    >
-                      Agregar
-                    </Button>
-                  </Stack>
-                  <FormErrorMessage>Partido inválido</FormErrorMessage>
-                </FormControl>
-              </form>
-
-              {!!input.matches.length &&
-                input.matches.map((el) => (
-                  <Box key={el.key}>
-                    <GridItem
-                      _hover={{
-                        bgColor: "#04879C",
-                      }}
-                      boxShadow="dark-lg"
-                      transition="200ms ease"
-                      backgroundColor="rgba(57,91,100,0.7)"
-                      borderRadius="20px"
-                      display={"flex"}
-                      p="5px"
-                      w="auto"
-                      margin="5px"
-                    >
-                      <Stack p="5px" spacing={3}>
-                        <Text fontSize="15px" fontWeight="bold" color="#AEFEFF">
-                          {el.team_a_name}
-                        </Text>
-                        <Text fontSize="15px" fontWeight="bold" color="#AEFEFF">
-                          {el.team_b_name}
-                        </Text>
-                        <Text fontSize="15px" fontWeight="bold" color="#AEFEFF">
-                          {el.date}
-                        </Text>
-                      </Stack>
-                      <Button value={el.key} onClick={quitarPartido}>
-                        X
-                      </Button>
-                    </GridItem>
-                  </Box>
-                ))}
-            </Box>
+            <MatchAdd cb={addMatch} equipos={input.teams} />
+            <Box></Box>
           </Stack>
         </Box>
       </Flex>
