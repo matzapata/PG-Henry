49b8e0241ff25fc45e3f39c8c4704ede97f9efee
Melina Maccio <melina.maccio416@gmail.com>
2022-08-24 21:16:02 -0300

    feat(client/src/components): add newsComponent

--- client/src/components/NewsCarousel.tsx
+++ client/src/components/NewsCarousel.tsx
@@ -1,15 +1,11 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
-import { color, Grid, IconButton, Image, Stack, Text } from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid, Stack, Text } from "@chakra-ui/react";
 import axios, { AxiosResponse } from "axios";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { ApiNew, New } from "../utils/newsInterfaces";
-import theme from "../utils/chakraTheme";
 
 function Carousel() {
-  const [loading, setLoading] = useState(false);
   const [news, setNews] = useState<New[]>([]);
-  const [selectecIndex, setSelectedIndex] = useState(0);
   const [selectedNew, setSelectedNew] = useState(news[0]);
 
   useEffect(() => {
@@ -26,60 +22,30 @@ function Carousel() {
             image: el.image_url,
           };
         });
-        console.log(dataMap);
         setNews(dataMap);
-        selectNew(selectecIndex, news);
       });
   }, []);
 
-  const selectNew = (index: number, news: New[], next = true) => {
-    const condition = next
-      ? selectecIndex < news.length - 1
-      : selectecIndex > 0;
-    const nextIndex = next
-      ? condition
-        ? selectecIndex + 1
-        : 0
-      : condition
-      ? selectecIndex - 1
-      : news.length - 1;
-    setSelectedNew(news[nextIndex]);
-    setSelectedIndex(nextIndex);
-  };
-  const previous = () => {
-    selectNew(selectecIndex, news, false);
-  };
-  const next = () => {
-    selectNew(selectecIndex, news);
-  };
-
-  console.log(news);
-  console.log(selectedNew);
-
   return (
-    <Grid templateColumns="repeat(3, 3fr)" gap={2} m="20px" pt="50px">
-      <IconButton
-        onClick={previous}
-        aria-label="previous"
-        icon={<ChevronLeftIcon />}
-      />
-      {/* <Stack
-        direction="row"
-        bgImage={selectedNew.image} 
-        width="80vw"
-        height="300px"
-      >
-        <Link to={selectedNew.link}>
-          <Text color="#F7F7F7">{selectedNew.title}</Text>
-          <Text color="#F7F7F7">{selectedNew.description}</Text>
-        </Link> 
-      </Stack> */}
-      <IconButton
-        onClick={next}
-        aria-label="next"
-        icon={<ChevronRightIcon />}
-      />
-    </Grid>
+    <Stack direction="row" height="50%" justifyContent="center">
+      <Stack bgImg={selectedNew?.image} bgSize="cover" width="65%">
+        <Heading>{selectedNew?.title}</Heading>
+        <Text>{selectedNew?.description}</Text>
+      </Stack>
+      <SimpleGrid columns={3} width="50%">
+        {news.map((e) => (
+          <Box
+            onClick={() => setSelectedNew(e)}
+            key={e.image}
+            bgImg={e.image}
+            height="10%"
+          >
+            <Heading fontSize="5px">{e.title}</Heading>
+            <Text>{e.description}</Text>
+          </Box>
+        ))}
+      </SimpleGrid>
+    </Stack>
   );
 }
 
