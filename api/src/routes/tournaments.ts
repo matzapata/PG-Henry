1147512fcbd503f91dffa7817239828eda1774a4
AlejandroGuil <alejandro.i.guil@gmail.com>
2022-08-25 14:24:21 -0300

    feat{api/src/routes/tournaments.ts}add/POST/create

--- api/src/routes/tournaments.ts
+++ api/src/routes/tournaments.ts
@@ -1,6 +1,22 @@
 import prisma from "../db";
 import * as express from "express";
-import { Status, TournamentType } from "@prisma/client";
+import { MatchStage, Status, TournamentType } from "@prisma/client";
+import db from "../db";
+
+type Team = {
+  name: string;
+  shield_url: string;
+  key: number;
+};
+
+type Match = {
+  date: string;
+  stage: MatchStage;
+  tournament_id: string;
+  team_a_name: string;
+  team_b_name: string;
+  key: number;
+};
 
 const router: express.Router = express.Router();
 
@@ -58,5 +74,107 @@ router.get("/:id", async (req: express.Request, res: express.Response) => {
     res.status(400).send({ message: e.message });
   }
 });
+router.post("/create", async (req: express.Request, res: express.Response) => {
+  try {
+    const {
+      name,
+      description,
+      user_limit,
+      creator_user_id,
+      type,
+      logo_url,
+      password,
+      teams,
+      matches,
+    } = req.body;
 
+    if ([name, description, user_limit, type].includes(undefined))
+      return res.status(400).send("Missing required parameters.");
+    let torneo: any;
+    if (type === "PUBLIC") {
+      torneo = await db.tournament.create({
+        data: {
+          name,
+          description,
+          user_limit,
+          creator_user_id,
+          type,
+          logo_url,
+        },
+      });
+    } else {
+      if (!password) return res.status(400).send("Password required.");
+      torneo = await db.tournament.create({
+        data: {
+          name,
+          description,
+          user_limit,
+          creator_user_id,
+          type,
+          logo_url,
+          password,
+        },
+      });
+    }
+    if (!teams.length) {
+      return res
+        .status(400)
+        .send("Missing required parameters. When Teams create");
+    } else {
+      /* const matchesPromises = matches.map(async (match: Match) => {
+        const newMatch = await db.matches.create({
+          data: { },
+        });
+        
+        return newMatch;
+      });
+      await Promise.all(matchesPromises); */
+
+      const teamsPromises = teams.map(async (team: Team) => {
+        const newTeam = await db.teams.create({
+          data: { shield_url: team.shield_url, name: team.name },
+        });
+
+        return newTeam;
+      });
+      await Promise.all(teamsPromises);
+      if (!matches.length) {
+        return res
+          .status(400)
+          .send("Missing required parameters. When Matches create");
+      } else {
+        const matchesPromises = matches.map(async (match: Match) => {
+          const newDate = match.date + "T00:00:00.000Z";
+          const team_a = await prisma.teams.findUnique({
+            where: { name: match.team_a_name },
+          });
+          const team_b = await prisma.teams.findUnique({
+            where: { name: match.team_b_name },
+          });
+          if (team_a && team_b) {
+            const newMatch = await db.matches.create({
+              data: {
+                tournament_id: torneo.id,
+                date: newDate,
+                stage: match.stage,
+                team_a_id: team_a?.id,
+                team_b_id: team_b?.id,
+              },
+            });
+            return newMatch;
+          } else {
+            return res
+              .status(400)
+              .send("Missing required parameters. Fail Team_a || team_b");
+          }
+        });
+        await Promise.all(matchesPromises);
+      }
+    }
+
+    res.status(200).json(torneo);
+  } catch (e: any) {
+    res.status(400).send({ message: e.message });
+  }
+});
 export default router;
