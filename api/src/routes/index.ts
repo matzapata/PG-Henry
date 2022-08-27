bb733a9d7afa051cbb2349b9b3a3357315cf80d2
Xxmbxxmb <diegocerdacelis@hotmail.com>
2022-08-27 12:19:44 -0400

    Fix(deleted useless team component)

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -3,7 +3,6 @@ import userRouter from "./users";
 import tournamentRouter from "./tournaments";
 import authRouter from "./auth";
 import mercadopagoRouter from "./mercadopago";
-import teamsRouter from "./teams";
 
 const router: express.Router = express.Router();
 
@@ -11,6 +10,5 @@ router.use("/users", userRouter);
 router.use("/auth", authRouter);
 router.use("/tournaments", tournamentRouter);
 router.use("/mercadopago", mercadopagoRouter);
-router.use("/teams", teamsRouter);
 
 export default router;
