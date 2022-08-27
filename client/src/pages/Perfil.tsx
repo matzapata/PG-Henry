71a02be57ba07af6969606f97174e9d05a6ee596
Xxmbxxmb <diegocerdacelis@hotmail.com>
2022-08-27 12:15:41 -0400

    Refactor(sequential search added)

--- client/src/pages/Perfil.tsx
+++ client/src/pages/Perfil.tsx
@@ -14,6 +14,7 @@ import {
   Center,
   Icon,
   Text,
+  Box,
 } from "@chakra-ui/react";
 import { CheckIcon, SmallCloseIcon } from "@chakra-ui/icons";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -27,6 +28,7 @@ import {
 import history from "../utils/history";
 import { signOut } from "../redux/slices/authThunk";
 import { FaExclamationCircle } from "react-icons/fa";
+import NavBar from "../components/NavBar";
 
 export default function UserProfileEdit(): JSX.Element {
   const { user, isAuthenticated, logout } = useAuth0();
@@ -58,280 +60,283 @@ export default function UserProfileEdit(): JSX.Element {
 
   return (
     <>
-      {isLoggedIn && isAuthenticated ? (
-        <Flex
-          h="900px"
-          p="0"
-          bgSize="cover"
-          bgImage="url('/img/landing-wallpaper.jpg')"
-          maxW={"100vw"}
-          align={"center"}
-          justify={"center"}
-        >
-          <Stack
-            spacing={4}
-            w={"full"}
-            maxW={"md"}
-            bg={useColorModeValue("white", "gray.700")}
-            rounded={"xl"}
-            boxShadow={"lg"}
-            p={6}
-            my={12}
+      <Box bgColor="primary">
+        <NavBar />
+        {isLoggedIn && isAuthenticated ? (
+          <Flex
+            h="900px"
+            p="0"
+            bgSize="cover"
+            bgImage="url('/img/landing-wallpaper.jpg')"
+            maxW={"100vw"}
+            align={"center"}
+            justify={"center"}
           >
-            <Heading lineHeight={1.1} fontSize={{ base: "2xl", sm: "3xl" }}>
-              Editar Perfil
-            </Heading>
-            <FormControl id="userName">
-              <Stack direction={["column", "row"]} spacing={6}>
-                <Center>
-                  <Avatar size="xl" src={user_detail.avatar}>
-                    <AvatarBadge
-                      as={IconButton}
-                      size="sm"
-                      rounded="full"
-                      top="-10px"
-                      colorScheme="red"
-                      aria-label="remove Image"
-                      icon={<SmallCloseIcon />}
+            <Stack
+              spacing={4}
+              w={"full"}
+              maxW={"md"}
+              bg={useColorModeValue("white", "gray.700")}
+              rounded={"xl"}
+              boxShadow={"lg"}
+              p={6}
+              my={12}
+            >
+              <Heading lineHeight={1.1} fontSize={{ base: "2xl", sm: "3xl" }}>
+                Editar Perfil
+              </Heading>
+              <FormControl id="userName">
+                <Stack direction={["column", "row"]} spacing={6}>
+                  <Center>
+                    <Avatar size="xl" src={user_detail.avatar}>
+                      <AvatarBadge
+                        as={IconButton}
+                        size="sm"
+                        rounded="full"
+                        top="-10px"
+                        colorScheme="red"
+                        aria-label="remove Image"
+                        icon={<SmallCloseIcon />}
+                      />
+                    </Avatar>
+                  </Center>
+                  <Center w="full">
+                    <UploadFiles
+                      funcion={"Cambiar Avatar"}
+                      titulo={"Subir Imagen"}
+                      url={"/users/changeavatar"}
                     />
-                  </Avatar>
-                </Center>
-                <Center w="full">
-                  <UploadFiles
-                    funcion={"Cambiar Avatar"}
-                    titulo={"Subir Imagen"}
-                    url={"/users/changeavatar"}
-                  />
-                </Center>
+                  </Center>
+                </Stack>
+              </FormControl>
+              <FormControl id="userName" isRequired>
+                <FormLabel>Usuario</FormLabel>
+                <Input
+                  placeholder="Nombre de usuario"
+                  _placeholder={{ color: "gray.500" }}
+                  type="text"
+                />
+              </FormControl>
+              <FormControl id="email" isRequired>
+                <FormLabel>Correo Electronico</FormLabel>
+                <Input
+                  placeholder="No disponible al ingresar con Auth0"
+                  _placeholder={{ color: "gray.500" }}
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  disabled
+                />
+              </FormControl>
+              <FormControl id="password" isRequired>
+                <FormLabel>Contraseña</FormLabel>
+                <Input
+                  placeholder="No disponible al ingresar con Auth0"
+                  _placeholder={{ color: "gray.500" }}
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  disabled
+                />
+              </FormControl>
+              <Stack spacing={6} direction={["column", "row"]}>
+                <Button
+                  bg={"red.400"}
+                  color={"white"}
+                  w="full"
+                  _hover={{
+                    bg: "red.500",
+                  }}
+                  onClick={() => {
+                    history.push("/");
+                  }}
+                >
+                  Cancelar
+                </Button>
+                <Button
+                  bg={"blue.400"}
+                  color={"white"}
+                  w="full"
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                  onClick={async () => {
+                    await dispatch(changePassword({ email, password }));
+                    setEmail("");
+                    setPassword("");
+                  }}
+                >
+                  Confirmar
+                </Button>
               </Stack>
-            </FormControl>
-            <FormControl id="userName" isRequired>
-              <FormLabel>Usuario</FormLabel>
-              <Input
-                placeholder="Nombre de usuario"
-                _placeholder={{ color: "gray.500" }}
-                type="text"
-              />
-            </FormControl>
-            <FormControl id="email" isRequired>
-              <FormLabel>Correo Electronico</FormLabel>
-              <Input
-                placeholder="No disponible al ingresar con Auth0"
-                _placeholder={{ color: "gray.500" }}
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                disabled
-              />
-            </FormControl>
-            <FormControl id="password" isRequired>
-              <FormLabel>Contraseña</FormLabel>
-              <Input
-                placeholder="No disponible al ingresar con Auth0"
-                _placeholder={{ color: "gray.500" }}
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                disabled
-              />
-            </FormControl>
-            <Stack spacing={6} direction={["column", "row"]}>
-              <Button
-                bg={"red.400"}
-                color={"white"}
-                w="full"
-                _hover={{
-                  bg: "red.500",
-                }}
-                onClick={() => {
-                  history.push("/");
-                }}
-              >
-                Cancelar
-              </Button>
-              <Button
-                bg={"blue.400"}
-                color={"white"}
-                w="full"
-                _hover={{
-                  bg: "blue.500",
-                }}
-                onClick={async () => {
-                  await dispatch(changePassword({ email, password }));
-                  setEmail("");
-                  setPassword("");
-                }}
-              >
-                Confirmar
-              </Button>
+              <Stack spacing={6} direction={["column", "row"]}>
+                <Button
+                  bg={"red.400"}
+                  color={"white"}
+                  w="full"
+                  _hover={{
+                    bg: "red.500",
+                  }}
+                  onClick={onDeleteUser}
+                >
+                  Eliminar Cuenta
+                </Button>
+              </Stack>
+              {error ? (
+                <Flex mt="4" alignItems="center" justifyContent="center">
+                  <Icon as={FaExclamationCircle} color="red.500" mr="2" />
+                  <Text as="span" color="red.500" fontWeight="500">
+                    {error}
+                  </Text>
+                </Flex>
+              ) : success ? (
+                <Flex mt="4" alignItems="center" justifyContent="center">
+                  <Icon as={CheckIcon} color="green.500" mr="2" />
+                  <Text as="span" color="green.500" fontWeight="500">
+                    {success}
+                  </Text>
+                </Flex>
+              ) : null}
             </Stack>
-            <Stack spacing={6} direction={["column", "row"]}>
-              <Button
-                bg={"red.400"}
-                color={"white"}
-                w="full"
-                _hover={{
-                  bg: "red.500",
-                }}
-                onClick={onDeleteUser}
-              >
-                Eliminar Cuenta
-              </Button>
-            </Stack>
-            {error ? (
-              <Flex mt="4" alignItems="center" justifyContent="center">
-                <Icon as={FaExclamationCircle} color="red.500" mr="2" />
-                <Text as="span" color="red.500" fontWeight="500">
-                  {error}
-                </Text>
-              </Flex>
-            ) : success ? (
-              <Flex mt="4" alignItems="center" justifyContent="center">
-                <Icon as={CheckIcon} color="green.500" mr="2" />
-                <Text as="span" color="green.500" fontWeight="500">
-                  {success}
-                </Text>
-              </Flex>
-            ) : null}
-          </Stack>
-        </Flex>
-      ) : isLoggedIn ? (
-        <Flex
-          h="900px"
-          p="0"
-          bgSize="cover"
-          bgImage="url('/img/landing-wallpaper.jpg')"
-          maxW={"100vw"}
-          align={"center"}
-          justify={"center"}
-        >
-          <Stack
-            spacing={4}
-            w={"full"}
-            maxW={"md"}
-            bg={useColorModeValue("white", "gray.700")}
-            rounded={"xl"}
-            boxShadow={"lg"}
-            p={6}
-            my={12}
+          </Flex>
+        ) : isLoggedIn ? (
+          <Flex
+            h="900px"
+            p="0"
+            bgSize="cover"
+            bgImage="url('/img/landing-wallpaper.jpg')"
+            maxW={"100vw"}
+            align={"center"}
+            justify={"center"}
           >
-            <Heading lineHeight={1.1} fontSize={{ base: "2xl", sm: "3xl" }}>
-              Editar Perfil
-            </Heading>
-            <FormControl id="userName">
-              <Stack direction={["column", "row"]} spacing={6}>
-                <Center>
-                  <Avatar size="xl" src={user_detail.avatar}>
-                    <AvatarBadge
-                      as={IconButton}
-                      size="sm"
-                      rounded="full"
-                      top="-10px"
-                      colorScheme="red"
-                      aria-label="remove Image"
-                      icon={<SmallCloseIcon />}
+            <Stack
+              spacing={4}
+              w={"full"}
+              maxW={"md"}
+              bg={useColorModeValue("white", "gray.700")}
+              rounded={"xl"}
+              boxShadow={"lg"}
+              p={6}
+              my={12}
+            >
+              <Heading lineHeight={1.1} fontSize={{ base: "2xl", sm: "3xl" }}>
+                Editar Perfil
+              </Heading>
+              <FormControl id="userName">
+                <Stack direction={["column", "row"]} spacing={6}>
+                  <Center>
+                    <Avatar size="xl" src={user_detail.avatar}>
+                      <AvatarBadge
+                        as={IconButton}
+                        size="sm"
+                        rounded="full"
+                        top="-10px"
+                        colorScheme="red"
+                        aria-label="remove Image"
+                        icon={<SmallCloseIcon />}
+                      />
+                    </Avatar>
+                  </Center>
+                  <Center w="full">
+                    <UploadFiles
+                      funcion={"Cambiar Avatar"}
+                      titulo={"Subir Imagen"}
+                      url={"/users/changeavatar"}
                     />
-                  </Avatar>
-                </Center>
-                <Center w="full">
-                  <UploadFiles
-                    funcion={"Cambiar Avatar"}
-                    titulo={"Subir Imagen"}
-                    url={"/users/changeavatar"}
-                  />
-                </Center>
+                  </Center>
+                </Stack>
+              </FormControl>
+              <FormControl id="userName" isRequired>
+                <FormLabel>Usuario</FormLabel>
+                <Input
+                  placeholder="Nombre de usuario"
+                  _placeholder={{ color: "gray.500" }}
+                  type="text"
+                />
+              </FormControl>
+              <FormControl id="email" isRequired>
+                <FormLabel>Correo Electronico</FormLabel>
+                <Input
+                  placeholder="Email"
+                  _placeholder={{ color: "gray.500" }}
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                />
+              </FormControl>
+              <FormControl id="password" isRequired>
+                <FormLabel>Contraseña</FormLabel>
+                <Input
+                  placeholder="Contraseña"
+                  _placeholder={{ color: "gray.500" }}
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                />
+              </FormControl>
+              <Stack spacing={6} direction={["column", "row"]}>
+                <Button
+                  bg={"red.400"}
+                  color={"white"}
+                  w="full"
+                  _hover={{
+                    bg: "red.500",
+                  }}
+                  onClick={() => {
+                    history.push("/");
+                  }}
+                >
+                  Cancelar
+                </Button>
+                <Button
+                  bg={"blue.400"}
+                  color={"white"}
+                  w="full"
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                  onClick={() => {
+                    dispatch(changePassword({ email, password }));
+                    setEmail("");
+                    setPassword("");
+                  }}
+                >
+                  Confirmar
+                </Button>
               </Stack>
-            </FormControl>
-            <FormControl id="userName" isRequired>
-              <FormLabel>Usuario</FormLabel>
-              <Input
-                placeholder="Nombre de usuario"
-                _placeholder={{ color: "gray.500" }}
-                type="text"
-              />
-            </FormControl>
-            <FormControl id="email" isRequired>
-              <FormLabel>Correo Electronico</FormLabel>
-              <Input
-                placeholder="Email"
-                _placeholder={{ color: "gray.500" }}
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-            </FormControl>
-            <FormControl id="password" isRequired>
-              <FormLabel>Contraseña</FormLabel>
-              <Input
-                placeholder="Contraseña"
-                _placeholder={{ color: "gray.500" }}
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
-            </FormControl>
-            <Stack spacing={6} direction={["column", "row"]}>
-              <Button
-                bg={"red.400"}
-                color={"white"}
-                w="full"
-                _hover={{
-                  bg: "red.500",
-                }}
-                onClick={() => {
-                  history.push("/");
-                }}
-              >
-                Cancelar
-              </Button>
-              <Button
-                bg={"blue.400"}
-                color={"white"}
-                w="full"
-                _hover={{
-                  bg: "blue.500",
-                }}
-                onClick={() => {
-                  dispatch(changePassword({ email, password }));
-                  setEmail("");
-                  setPassword("");
-                }}
-              >
-                Confirmar
-              </Button>
-            </Stack>
-            <Stack spacing={6} direction={["column", "row"]}>
-              <Button
-                bg={"red.400"}
-                color={"white"}
-                w="full"
-                _hover={{
-                  bg: "red.500",
-                }}
-                onClick={onDeleteUser}
-              >
-                Eliminar Cuenta
-              </Button>
-            </Stack>
+              <Stack spacing={6} direction={["column", "row"]}>
+                <Button
+                  bg={"red.400"}
+                  color={"white"}
+                  w="full"
+                  _hover={{
+                    bg: "red.500",
+                  }}
+                  onClick={onDeleteUser}
+                >
+                  Eliminar Cuenta
+                </Button>
+              </Stack>
 
-            {error ? (
-              <Flex mt="4" alignItems="center" justifyContent="center">
-                <Icon as={FaExclamationCircle} color="red.500" mr="2" />
-                <Text as="span" color="red.500" fontWeight="500">
-                  {error}
-                </Text>
-              </Flex>
-            ) : success ? (
-              <Flex mt="4" alignItems="center" justifyContent="center">
-                <Icon as={CheckIcon} color="green.500" mr="2" />
-                <Text as="span" color="green.500" fontWeight="500">
-                  {success}
-                </Text>
-              </Flex>
-            ) : null}
-          </Stack>
-        </Flex>
-      ) : null}
+              {error ? (
+                <Flex mt="4" alignItems="center" justifyContent="center">
+                  <Icon as={FaExclamationCircle} color="red.500" mr="2" />
+                  <Text as="span" color="red.500" fontWeight="500">
+                    {error}
+                  </Text>
+                </Flex>
+              ) : success ? (
+                <Flex mt="4" alignItems="center" justifyContent="center">
+                  <Icon as={CheckIcon} color="green.500" mr="2" />
+                  <Text as="span" color="green.500" fontWeight="500">
+                    {success}
+                  </Text>
+                </Flex>
+              ) : null}
+            </Stack>
+          </Flex>
+        ) : null}
+      </Box>
     </>
   );
 }
