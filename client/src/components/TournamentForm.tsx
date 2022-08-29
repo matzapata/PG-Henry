4d4c9207ffc49470b5a351f45d7d2b1ef35a8949
AlejandroGuil <alejandro.i.guil@gmail.com>
2022-08-29 11:51:11 -0300

    refactor{clinet/src/components/TournamnetForm.tsx}

--- client/src/components/TournamentForm.tsx
+++ client/src/components/TournamentForm.tsx
@@ -237,7 +237,7 @@ export default function TournamentForm(): JSX.Element {
                     (errors.user_limit === "Completado" ||
                       errors.user_limit === "") &&
                     (errors.password === "Completado" || errors.password === "")
-                      ? "white"
+                      ? "black"
                       : "red.500"
                   }
                 >
@@ -246,7 +246,7 @@ export default function TournamentForm(): JSX.Element {
                 <Tab
                   textColor={
                     errors.teams === "Completado" || errors.teams === ""
-                      ? "white"
+                      ? "black"
                       : "red.500"
                   }
                 >
@@ -255,7 +255,7 @@ export default function TournamentForm(): JSX.Element {
                 <Tab
                   textColor={
                     errors.matches === "Completado" || errors.matches === ""
-                      ? "white"
+                      ? "black"
                       : "red.500"
                   }
                 >
