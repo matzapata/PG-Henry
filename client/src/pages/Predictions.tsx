724aed7bd0ed27aab40154d859c6a69e6d9940c4
AlejandroGuil <alejandro.i.guil@gmail.com>
2022-09-02 11:25:14 -0300

    modified:   client/src/pages/Predictions.tsx 	modified:   client/src/pages/TournamentDetail.tsx

--- client/src/pages/Predictions.tsx
+++ client/src/pages/Predictions.tsx
@@ -1,10 +1,15 @@
-import { Container, Flex, Heading } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 import React from "react";
+import { useParams } from "react-router-dom";
+import AddPrediction from "../components/AddPrediction";
 
 function Predictions() {
+  const { id } = useParams<{ id: string }>();
   return (
     <Flex backgroundColor={"primary"} width={"100vw"} height={"100vh"}>
       <Heading color={"text"}>Prode</Heading>
+
+      <AddPrediction id={id} />
     </Flex>
   );
 }
